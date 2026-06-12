48a9f6cf2b9f08688fb0bc34d33e673501c0ce71
saddab <saddabakhtar17@gmail.com>
2026-06-12 15:32:36 +0530

    feat(ai-service): implement recommendation service with MongoDB integration

---
+++ AI-Service/src/main/java/com/sadcodes/aiservice/repository/RecommendationRepository.java
@@ -0,0 +1,13 @@
+package com.sadcodes.aiservice.repository;
+
+import com.sadcodes.aiservice.model.Recommendation;
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface RecommendationRepository extends MongoRepository<Recommendation,String > {
+    List<Recommendation> findByUserId(String userId);
+
+   Optional< Recommendation> findByActivityId(String activityId);
+}
