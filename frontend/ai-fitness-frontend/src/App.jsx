a66a3a557175c67b7ffabe83539e93c467a9edfc
saddab <saddabakhtar17@gmail.com>
2026-08-03 15:19:33 +0530

    feat(frontend): integrate Keycloak PKCE authentication

--- frontend/ai-fitness-frontend/src/App.jsx
+++ frontend/ai-fitness-frontend/src/App.jsx
@@ -1,12 +1,39 @@
 import Button from "@mui/material/Button";
+import { useContext, useEffect, useState } from "react";
+import { AuthContext } from "react-oauth2-code-pkce";
+import { useDispatch } from "react-redux";
 import { BrowserRouter as Router } from "react-router";
+import { setCredentials } from "./store/authSlice";
 
 export default function App() {
+  const { token, tokenData, logIn, logout, isAuthenticated } =
+    useContext(AuthContext);
+
+  const dispatch = useDispatch();
+  const [authRead, setAuthReady] = useState(false);
+
+  useEffect(() => {
+    if (token) {
+      dispatch(setCredentials({ token, user: tokenData }));
+      setAuthReady(true);
+    }
+  }, [token, tokenData, dispatch]);
   return (
     <Router>
-      <Button variant="contained">
-        LOGIN
-      </Button>
+      {!token ? (
+        <Button
+          variant="contained"
+          onClick={() => {
+            logIn();
+          }}
+        >
+          LOGIN
+        </Button>
+      ) : (
+        <div>
+          <pre>{JSON.stringify(tokenData, null, 2)}</pre>
+        </div>
+      )}
     </Router>
   );
 }
--- frontend/ai-fitness-frontend/src/main.jsx
+++ frontend/ai-fitness-frontend/src/main.jsx
@@ -1,12 +1,16 @@
 import ReactDOM from "react-dom/client";
 import { Provider } from "react-redux";
+import {AuthProvider} from 'react-oauth2-code-pkce'
 import App from "./App";
 import { store } from "./store/store";
+import { authConfig } from "./authConfig";
 
 // As of React 18
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <Provider store={store}>
-    <App />
-  </Provider>,
+  <AuthProvider authConfig={authConfig}>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </AuthProvider>
 );
